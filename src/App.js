1d810befc36176b8e6cf5d2d5b08c78173bd0eb5
saksh-aam <sakshamkathuria20@cse.iiitp.ac.in>
2023-01-21 06:33:32 +0000

    Handled edge case and solved some bugs

--- src/App.js
+++ src/App.js
@@ -3,11 +3,19 @@ import produce from "immer";
 import "./App.css";
 
 const random = (counter, setboard) => {
+  let count=0;
   setboard(
     Array.from({ length: counter }, () =>
-      Array.from({ length: counter }, () => Math.floor(Math.random() * 1.5))
+      Array.from({ length: counter }, () => {
+        let cellval=Math.random() * 1.5;
+        if(cellval===0) count=count+1;
+        return Math.floor(cellval);
+      })
     )
   );
+  if(count===counter*counter){
+    random(counter, setboard);
+  }
 };
 
 const stopped=(stop, tempgrid, setboard)=>{
@@ -46,6 +54,24 @@ const nextlevel=(next, setnext, stop, counter, setcounter, setrunning)=>{
   }
 }
 
+const setdefault = (counter, setvalid, setcopy, setnext, setcreate, setrunning, setstop, setboard, setgrid)=>{
+  setboard(
+    Array.from({ length: counter }, () =>
+      Array.from({ length: counter }, () => 0)
+    )
+  );
+  setgrid(
+    Array.from({ length: counter }, () =>
+      Array.from({ length: counter }, () => 0)
+    )
+  );
+  setvalid(0);
+  setcopy(false);
+  setcreate(false);
+  setnext(false);
+  setstop(false);
+  setrunning(false);
+}
 function App() {
   const [counter, setcounter] = useState(0);
   const [valid, setvalid] = useState(0);
@@ -65,16 +91,20 @@ function App() {
       Array.from({ length: counter }, () => 0)
     )
   );
-
+  
   useEffect(() => {
-    random(counter, setboard);
-    setcopy(true);
+    if(counter!==0){
+      random(counter, setboard);
+      const root = document.documentElement;
+      root?.style.setProperty("--counter", counter);
+      setcopy(true);
+    }
   }, [counter]);
 
-  useEffect(() => {
-    const root = document.documentElement;
-    root?.style.setProperty("--counter", counter);
-  }, [counter]);
+  // useEffect(() => {
+  //   const root = document.documentElement;
+  //   root?.style.setProperty("--counter", counter);
+  // }, [counter]);
 
   useEffect(() => {
     if (copy) {
@@ -93,7 +123,7 @@ function App() {
           }
         }
       }setvalid(count);
-      setTimeout(() => {
+      var timer = setTimeout(() => {
         setboard(
           Array.from({ length: counter }, () =>
             Array.from({ length: counter }, () => 0)
@@ -103,24 +133,32 @@ function App() {
         setcreate(false);
         setrunning(false);
       }, 3000);
+
     }// eslint-disable-next-line
   }, [create]);
 
   return (
     <div className="App">
       <h1>Let's check your Memory</h1>
+     <> {counter===0?
       <button
-        onClick={() => {
-          setcounter(3);
-          setstop(false);
-          setrunning(true);
-        }}
-      >
-        Start
-      </button>
+      onClick={() => {
+        setcounter(3);
+        setstop(false);
+        setrunning(true);
+      }}
+    >
+      Start
+    </button>:
+    <button
+    onClick={()=>{setcounter(0); setdefault(counter, setvalid, setcopy, setnext, setcreate, setrunning, setstop, setboard, setgrid);}}
+  >
+    Restart
+  </button>}</>
       {nextlevel(next, setnext, stop, counter, setcounter, setrunning)}
 
-      <div className="playboard">
+      <>{
+        counter!==0? <div className="playboard">
         {board.map((x, i) =>
           x.map((_, j) => (
             <div
@@ -146,7 +184,9 @@ function App() {
             ></div>
           ))
         )}
-      </div>
+      </div>:<></>
+      }
+      </>
       <br />
       {stopped(stop, tempgrid, setboard)}
     </div>
